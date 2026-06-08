def7127283ee044631c21a94e0f0e19043cb7234
Vuixxi <62472777+vuixxi@users.noreply.github.com>
2026-06-09 01:13:53 +0700

    Add files via upload

--- js/app/init.js
+++ js/app/init.js
@@ -1,4 +1,12 @@
-import { getAppData } from "../data/loader.js";
+import { security } from "./../ui/security.js";
+import { getAppData } from "./../data/loader.js";
+import { handleSidebar, handleContent, handleContentClose } from "./../ui/sidebar/index.js";
+import { initCalculatorTabs, initCalculator } from "./../ui/sidebar/calculator.js";
+import { handleFeedback } from "./../ui/sidebar/feedback.js";
+import { handleReport } from "./../ui/sidebar/report.js";
+import { getData, setupSummaryToggle } from "./../ui/summaryTable.js";
+import { handleDropDown } from "./../ui/dropdown.js";
+import { handleMain } from "./../ui/mainTable.js";
 
 async function startApp() {
   const appData = await getAppData();
@@ -14,21 +22,20 @@ startApp();
 
 
 function initApp(data, period, workerMap, jobMap) {
+  
+  security();
+  
   getData(data, period, workerMap, jobMap);
-
   handleDropDown(data, period, workerMap, jobMap);
+  
   handleSidebar();
   handleContent();
   handleContentClose();
-
   const calculate = initCalculator();
   initCalculatorTabs(calculate);
-
   handleFeedback();
   handleReport();
 
   handleMain(data, period, workerMap, jobMap);
-
   setupSummaryToggle();
 }
-
