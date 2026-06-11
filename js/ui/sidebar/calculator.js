18ecb47be2f2fb0f99af5e4379a1ca40dcbdab28
Vuixxi <62472777+vuixxi@users.noreply.github.com>
2026-06-11 17:34:57 +0700

    Add files via upload

--- js/ui/sidebar/calculator.js
+++ js/ui/sidebar/calculator.js
@@ -1,22 +1,6 @@
 import { formatNumber } from "./../../utils/format.js";
 
 // =======================
-// CONSTANT
-// =======================
-const RATE = {
-  "produksi": 3600,
-  "muat": 110000,
-  "bongkar": 120000,
-  "angkut cacahan lembut": 400,
-  "stapel cacahan": 800,
-  "stapel coa": 400,
-  "stapel produksi": 400,
-  "salin": 765,
-  "salin timbang": 200000
-}
-
-
-// =======================
 // LOGIC (PURE)
 // =======================
 function calculateResult(job, worker, rate) {
@@ -24,12 +8,13 @@ function calculateResult(job, worker, rate) {
   return (job * rate) / worker;
 }
 
-function getActiveRate() {
+function getActiveRate(jobMap) {
   const activeTab = document.querySelector(".calculator__tab.u-button--active");
   if (!activeTab) return 0;
-
-  const key = activeTab.innerText.toLowerCase();
-  return RATE[key] || 0;
+  
+  const jobId =activeTab.dataset.jobId;
+  
+  return jobMap[jobId].rate || 0;
 }
 
 
@@ -47,15 +32,17 @@ export function initCalculatorTabs(calculateCallback) {
   });
 }
 
-export function initCalculator() {
+export function initCalculator(jobMap) {
   const jobInput = document.querySelector(".calculator__input-job");
   const workerInput = document.querySelector(".calculator__input-worker");
   const output = document.querySelector(".calculator__output");
+  
+  renderCalculatorTab(jobMap);
 
   function calculate() {
     const job = Number(jobInput.value);
     const worker = Number(workerInput.value);
-    const rate = getActiveRate();
+    const rate = getActiveRate(jobMap);
 
     const result = calculateResult(job, worker, rate);
     renderCalculatorOutput(output, result);
@@ -68,9 +55,19 @@ export function initCalculator() {
 }
 
 
+// new
+function renderCalculatorTab(jobMap) {
+  let nav = document.querySelector(".calculator__navbar");
+  Object.values(jobMap).forEach(job => {
+    nav.innerHTML += `<button class="calculator__tab u-button" data-job-id="${job.id}">${job.name}</button>`;
+  });
+}
+
+
 // =======================
 // UI
 // =======================
+
 function renderCalculatorOutput(output, result) {
   output.innerText = result ? formatNumber(result) : "0";
 }
