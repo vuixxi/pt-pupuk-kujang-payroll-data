814acc93d3138972f3199e0a4f5a8d8e40e4ffd3
Vuixxi <62472777+vuixxi@users.noreply.github.com>
2026-06-13 11:48:24 +0700

    Add files via upload

---
+++ init.js
@@ -0,0 +1,56 @@
+import { loadAppComponents } from "./components.js";
+
+import { getAppData } from "./../services/loader.js";
+
+import { security } from "./../features/auth/login.js";
+
+import { handleDropDown } from "./../features/period/dropdown.js";
+
+import { handleMain } from "./../features/payroll/mainTable.js";
+import { getData, setupSummaryToggle } from "./../features/payroll/summaryTable.js";
+
+import { handleSidebar, handleContent, handleContentClose } from "./../features/sidebar/index.js";
+import { initCalculatorTabs, initCalculator } from "./../features/sidebar/calculator.js";
+import { handleFeedback } from "./../features/sidebar/feedback.js";
+import { handleReport } from "./../features/sidebar/report.js";
+
+import { initFooterVersion } from "./../features/ui/footerVersion.js";
+
+async function startApp() {
+
+  await loadAppComponents();
+  
+  const appData = await getAppData();
+
+  if (!appData) return;
+
+  const { data, period, workerMap, jobMap } = appData;
+
+  initApp(data, period, workerMap, jobMap);
+}
+
+startApp();
+
+
+function initApp(data, period, workerMap, jobMap) {
+  
+  security();
+  
+  getData(data, period, workerMap, jobMap);
+  handleDropDown(data, period, workerMap, jobMap);
+  
+  // handleSidebar() dipindah ke security,js (bug pada lebar sidebar)
+  
+  // handleSidebar();
+  handleContent();
+  handleContentClose();
+  const calculate = initCalculator(jobMap);
+  initCalculatorTabs(calculate);
+  handleFeedback();
+  handleReport();
+
+  handleMain(data, period, workerMap, jobMap);
+  setupSummaryToggle();
+  
+  initFooterVersion();
+}
